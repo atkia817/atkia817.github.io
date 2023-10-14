43af563183b503ecad3677d923db87c915c6d11a
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-10-14 23:48:16 +0600

    Ading changes for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,284 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faGraduationCap, faMicroscope } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const data = {
+    name: "Jeba Atkia Maisha",
+    bio: "A motivated individual with an interest in academic research focusing on different aspects of therapeutics designed for human diseases. Specific areas of interest include immunology, genomics and molecular biology.",
+    education: [
+      {
+        institute: "University of Manitoba",
+        location: "Winnipeg, Manitoba, Canada",
+        degree: "Master of Science in Immunology",
+        period: "Sep 2023 - Present",
+      },
+      {
+        institute: "BRAC University",
+        location: "Dhaka, Bangladesh",
+        degree: "Bachelor of Science in Biotechnology",
+        period: "Oct 2020",
+        grade: "CGPA:3.95",
+      },
+      {
+        institute: "Rajuk Uttara Model College",
+        location: "Dhaka, Bangladesh",
+        degree: "Higher Secondary Certificate",
+        period: "Aug 2015",
+        grade: "GPA: 5.00",
+      },
+      {
+        institute: "Rajuk Uttara Model College",
+        location: "Dhaka, Bangladesh",
+        degree: "Secondary School Certificate",
+        period: "May 2013",
+        grade: "GPA: 5.00",
+      },
+    ],
+    research: [
+      {
+        label: "Graduate (MSc) Thesis Research",
+        content: "Investigating the influence of cigarette smoking on neutrophilic function and inflammation. (ongoing)",
+      },
+      {
+        label: "Undergraduate Thesis Research",
+        content:
+          "A Study on the Frequency of HBBP1 rs2071348 Polymorphism and its Association with HbF Level and Disease Severity among Bangladeshi HbE/β - Thalassemia Patients.",
+      },
+    ],
+    lab: [
+      {
+        label: "PCR & qPCR",
+        content:
+          "Preparation of PCR master mix (both kit-based and individual reagent based), calibration and programming different PCRs. (equipment: Bio-Rad T100 thermal cycler)",
+      },
 
-    const data = {
-        name: "Jeba Atkia Maisha",
-        education: [
-            {
-                institute: "University of Manitoba",
-                location: "Winnipeg, Manitoba, Canada",
-                degree: "Master of Science in Immunology",
-                period: "Sep 2023 - Present",
-            },
-            {
-                institute: "BRAC University",
-                location: "Dhaka, Bangladesh",
-                degree: "Bachelor of Science in Biotechnology",
-                period: "Oct 2020",
-                grade: "CGPA: 3.95",
-            },
+      {
+        label: "Agarose Gel Electrophoresis",
+        content:
+          "Preparation of gel, casting of gel, subsequent gel imaging and analysis. (equipment: Bio-Rad PowerPac, Bio-Rad Sub-Cell GT, Bio-Rad GelDoc XR+)",
+      },
+      {
+        label: "HRM",
+        content:
+          "High resolution melt curve analysis from real time PCR results. (equipment: Bio-Rad CFX96 Touch Real-Time PCR Detection System)",
+      },
+      {
+        label: "DNA Isolation",
+        content: "Extraction (kit-based) of DNA from given samples such has human blood",
+      },
+      {
+        label: "DNA purification",
+        content: "Purification (kit-based) of isolated DNA from given samples",
+      },
+      {
+        label: "DNA quantification",
+        content:
+          "Quantification of purified DNA using a spectrophotometer. (equipment – BioTek EON Microplate Spectrophotometer)",
+      },
+      {
+        label: "Hematological Analysis",
+        content: "Quantitative analysis of blood such as CBC. (equipment - Sysmex KX-21)",
+      },
+      {
+        label: "Hemoglobin Electrophoresis",
+        content: "Detection of hemoglobin types present in a given blood sample. (equipment – Sebia Capillarys 2)",
+      },
+      {
+        label: "Antimicrobial Susceptibility Test",
+        content: "Determination of possible drug resistance by testing bacterial isolates",
+      },
+      {
+        label: "Neutrophil Isolation",
+        content: "Isolation of neutrophils from whole blood using ficoll-dextran method and RBC depletion kit method",
+      },
+      {
+        label: "Flow Cytometry",
+        content: "Preparation of cell samples for flow cytometry, cell fixing and cell staining. (equipment – BD LSRFortessa)",
+      },
+      {
+        label: "Immunofluorescence Imaging",
+        content:
+          "Preparation of coverslips, fixing and staining of coverslips for immunofluorescence imaging. (equipment – Zeiss)",
+      },
+    ],
+    awards: [
+      {
+        label: "Pro-Vice Chancellor’s Certificate for Residential Semester",
+        content:
+          "Awarded for academic excellence, high standard of discipline and devotion to duties during Residential Semester 44 (Brac University)",
+      },
+      {
+        label: "Academic Performance Based Scholarships",
+        content: "Consistent positions on the Vice-Chancellor’s list and the Dean’s List for semester wise performance.",
+      },
+      {
+        label: "TechCamp Dhaka Graduate 2018",
+        content:
+          "Successful completion of a 2-day long workshop on basicknowledge of software application development, marketing and management of the software application.",
+      },
+      {
+        label: "Bangladesh Youth Leadership Centre YLS Graduate 2016",
+        content:
+          "Successful completion of a 3-day long international youth summit discussing the current global issues focusing on the SDGs.",
+      },
+      {
+        label: "SpellChamps Online Spelling Competition 2016",
+        content: "Winner",
+      },
+      {
+        label: "Poster Presentation Competition “Converge” Nature Fest RUMC 2013",
+        content: "Winner",
+      },
+    ],
+    extracurricular: [
+      {
+        label: "Volunteer",
+        content:
+          "PIIN Conference 2023: Facilitated “poster presentation” section of the conference by arranging for and assisting in poster display.",
+      },
+      {
+        label: "Student Mentor",
+        content:
+          "Welcome Mentor Program, Student Affairs, University of Manitoba (Fall’23): Assisting international students for a smooth transition to the university life by providing relevant information, academic and non-academic support.",
+      },
+      {
+        label: "General Secretary",
+        content:
+          "Brac University Natural Sciences Club (Spring’19 – Spring’20): Supervision and execution of club activities, management of human resources etc.",
+      },
+      {
+        label: "Director of Marketing",
+        content:
+          "Brac University Natural Sciences Club (Spring’18 – Spring’19): Implementation of online and offline marketing strategies for showcasing club activities",
+      },
+      {
+        label: "Student Mentor",
+        content:
+          "Academic Advising Program for Freshers (Spring ’18 – Summer'19): Assisting freshers for a smooth transition to the university life by providing relevant information, academic and non-academic support.",
+      },
+      {
+        label: "Organizer",
+        content: "Rajuk Nationals Debate Competition 2016",
+      },
+      {
+        label: "Organizer",
+        content: "Rajuk Nature Fest 2014",
+      },
+      {
+        label: "Volunteer",
+        content: "Rajuk BDC Pre Worlds Debate Competition 2013",
+      },
+    ],
+    internship:
+      "Hands-on training on laboratory safety measurements, laboratory equipment uses and care, molecular biology procedures, microbiology procedures, paper presentation and scientific writing at the Institute for Developing Science and Health Initiatives (ideSHi), laboratory of Dr. Firdausi Qadri. – 2019",
+    appointments: [
+      {
+        label: "Medical Data Processor",
+        content:
+          "Augmedix BD (February’21 – August’22): Analysis and interpretation of patient data from electronic health records of the US, subsequent input of the processed data to an optimized medium.",
+      },
+      {
+        label: "Part-time Instructor",
+        content:
+          "Upskill (October’20 – February’21): Teaching Mathematics and Science to middle school level kids in an interactive system through the skill sharing platform of Upskill.",
+      },
+      {
+        label: "Executive (Community Management)",
+        content:
+          "TRI Digital Technologies Ltd. (July’20 – February’21): Management of social media platforms, query, CSR activities of clients, and implementation of marketing campaigns for the company’s extension – a cloud kitchen.",
+      },
+    ],
+    languages: [
+      {
+        label: "Bengali",
+        content: "Native",
+      },
+      {
+        label: "English",
+        content: "Fluent (IELTS Score - 8.0)",
+      },
+    ],
+    computerSkills: [
+      "MS Office Suite (MS Word, MS Excel, MS Powerpoint",
+      "Scientific Graphic and Statistical Analysis (GraphPad Prism)",
+      "Chromas (Sanger Sequence Viewer & Editor",
+    ],
+    scientificPoster: [
+      "“Effects of cigarette smoke extract on the functions of neutrophils” – PIIN Conference 2023",
+      "“Persistent Cigarette Smoking Is Associated with Rheumatoid Arthritis Onset and Neutrophil Activation in a Prospective Study of At-risk First-Degree Relatives” – ACR Convergence 2023",
+      "“Detection of common α-thalassemia deletions and α-globin gene triplication by molecular techniques in Bangladeshi population” – BSBMB-SABC International Conference 2020",
+    ],
+    publications: [
+      "Mostaid MS, Aziz MA, Maisha JA, Islam MS, Maruf AA. A review of pharmacogenetic studies in the Bangladeshi population. Drug Metab Pers Ther. 2023 Mar 1;38(2):123-131. doi: 10.1515/dmpt-2022-0194. PMID: 36854045",
+      "Maisha JA, El-Gabalawy HS, O'Neil LJ. Modifiable risk factors linked to the development of rheumatoid arthritis: evidence, immunological mechanisms and prevention. Front Immunol. 2023 Sep 12;14:1221125. doi: 10.3389/fimmu.2023.1221125. PMID: 37767100; PMCID: PMC10520718",
+    ],
+    academicReferences: [
+      {
+        name: "Dr. Liam O'Neil",
+        description: [
+          "MD, MHSc, FRCPC",
+          "Assistant Professor",
+          "Department of Immunology and Internal Medicine",
+          "University of Manitoba, Winnipeg, Manitoba.",
+          "E-mail: liam.oneil@umanitoba.ca",
         ],
-    };
+      },
+      {
+        name: "Dr. Iftekhar Bin Naser",
+        description: [
+          "Assistant Professor & Program Coordinator",
+          "Biotechnology Program",
+          "Department of Mathematics & Natural Sciences",
+          "Brac University.",
+          "E-mail: iftekhar.naser@bracu.ac.bd",
+        ],
+      },
+    ],
+  };
 
-    return (
-        <div className="main">
-            <div className="page_left">Jeba</div>
-        </div>
-    );
+  return (
+    <div className="main">
+      <div className="main_title">{data?.name}</div>
+      <div className="main_image">
+        <img className="main_image_img" src="/profile.jpg" alt="" />
+      </div>
+      <div className="main_bio">{data.bio}</div>
+      <div className="main_education">
+        <span className="main_section_header">
+          <span>Education</span>
+          <FontAwesomeIcon icon={faGraduationCap} />
+        </span>
+        {data.education.map(({ institute, location, degree, period, grade }, i) => {
+          return (
+            <div key={i} className="main_education_item">
+              <span className="main_education_item_institute">{institute}</span>
+              <span className="main_education_item_location">{location}</span>
+              <span className="main_education_item_degree">{degree}</span>
+              <span className="main_education_item_period">{period}</span>
+              <span className="main_education_item_grade">{grade}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="main_research">
+        <span className="main_section_header">
+          <span>Academic Research</span>
+          <FontAwesomeIcon icon={faMicroscope} />
+        </span>
+        {data.research.map(({ label, content }, i) => {
+          return (
+            <div key={i} className="main_research_item">
+              <span className="main_research_item_number">{i + 1}. </span>
+              <span className="main_research_item_label">{label}</span>
+              <span className="main_research_item_content">{content}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
